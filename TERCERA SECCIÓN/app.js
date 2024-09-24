dda16f9998fee5c0f726a5359b559cf897dea5bc
José Luis Hernández Claros <joseluishc1966@gmail.com>
2024-09-24 18:23:39 -0500

    Modulo eventos

--- TERCERA SECCIÓN/app.js
+++ TERCERA SECCIÓN/app.js
@@ -1,3 +1,14 @@
-const eventEmitter = require("events");
+//MODULO EVENTS
 
-console.log(eventEmitter);
+const EventEmitter = require("events");
+
+const emisorProductos = new EventEmitter();
+
+//definir eventos
+emisorProductos.on("compra", (total, numProducts) => {
+  console.log(`Total de la compra $${total}`);
+  console.log(`Numero de productos ${numProducts}`);
+});
+
+//activar eventos para que se ejecuten
+emisorProductos.emit("compra", 500, 45);
